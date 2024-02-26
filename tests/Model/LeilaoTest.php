5762947d0021b6789f3688670f329d28c39641c7
wellysonlee <martinsgwellyson@gmail.com>
2024-02-26 16:08:07 -0300

    mais de 5 testes por usuario

--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -9,6 +9,41 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoTest extends TestCase
 {
+    public function testLeilaoNaoDeveReceberLancesRepetidos()
+    {
+        $leilao = new Leilao('Variante');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
+
+    public function testLeilaoNaoDeveAceitar5lancesPorUsuario ()
+    {
+        $leilao = new Leilao('Brasília Amarela');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 1500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 3000));
+        $leilao->recebeLance(new Lance($maria, 3500));
+        $leilao->recebeLance(new Lance($joao, 4000));
+        $leilao->recebeLance(new Lance($maria, 4500));
+        $leilao->recebeLance(new Lance($joao, 5000));
+        $leilao->recebeLance(new Lance($maria, 5500));
+
+        $leilao->recebeLance(new Lance($joao, 6000));
+
+        static::assertCount(10, $leilao->getLances());
+        static::assertEquals(5500, $leilao->getLances()[array_key_last($leilao->getLances())]->getValor());
+    }
+    
     /**
      * @dataProvider geraLances
      */
